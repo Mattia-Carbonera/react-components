73a727247b7d411b75e127f2150daa32a67534fb
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-28 15:53:04 +0100

    add main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,26 @@
 import style from "./main.module.css";
+import defaultImage from "../../assets/img/card-image.png";
 
 function Main() {
   return (
     <main>
-      <p>prova</p>
+      <div className={style.card}>
+        <div className="image-container">
+          <img src={defaultImage} alt="card image" className={style.img} />
+        </div>
+        <div className={style.description}>
+          <h2>Titolo del post</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            optio quibusdam facilis, quaerat accusamus molestiae repellendus
+            odio error expedita. Soluta dignissimos hic optio voluptas culpa
+            molestias, est facere veritatis veniam!
+          </p>
+          <div className="button">
+            <button>prova</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
