f3ac1affb7e295e137fd70eeec3328fbea2c7998
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-28 17:46:31 +0100

    add card component & fix button

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,27 +1,10 @@
-import style from "./main.module.css";
-import Button from "../button/button";
+import Card from "../card/card";
 import defaultImage from "../../assets/img/card-image.png";
 
 function Main() {
   return (
     <main>
-      <div className={style.card}>
-        <div className="image-container">
-          <img src={defaultImage} alt="card image" className={style.img} />
-        </div>
-        <div className={style.description}>
-          <h2>Titolo del post</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            optio quibusdam facilis, quaerat accusamus molestiae repellendus
-            odio error expedita. Soluta dignissimos hic optio voluptas culpa
-            molestias, est facere veritatis veniam!
-          </p>
-          <div className="button">
-            <Button />
-          </div>
-        </div>
-      </div>
+      <Card />
     </main>
   );
 }
